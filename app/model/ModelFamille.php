25d691b1c7ec239d4fe570064aa56a63581596c0
laurepg <106607375+laurepg@users.noreply.github.com>
2022-06-05 19:41:51 +0200

    Update ModelFamille.php

--- app/model/ModelFamille.php
+++ app/model/ModelFamille.php
@@ -1,36 +1,113 @@
-
-<!-- ----- debut ModelFamille -->
+<!-- ----- début ModelFamille -->
 
 <?php
-require_once 'Model.php';
+require_once 'Model.php' ;
 
 class ModelFamille {
-    private $id,$nom;
-    public function __construct($id=NULL, $nom=NULL) {
-  // valeurs nulles si pas de passage de parametres
-  if (!is_null($id)) {
-   $this->id = $id;
-   $this->nom = $nom;
-  }
- }
-
- function setId($id) {
-  $this->id = $id;
- }
-
- function setNom($nom) {
-  $this->nom = $nom;
- }
-
-function getId() {
-  return $this->id;
- }
- 
- function getNom() {
-  return $this->nom;
- }
-
+    private $id, $nom ;
     
+    // pas possible d'avoir 2 constructeurs
+    public function __construct($id = NULL, $nom = NULL) {
+        // valeurs nulles si pas de passage de parametres
+        if (!is_null($id)) {
+            $this->id = $id ;
+            $this->nom = $nom ;
+        }
+    }
+    
+    function setId($id) {
+        $this->id = $id ;
+    }
+    
+    function setNom($nom) {
+        $this->nom = $nom ;
+    }
+    
+    function getId() {
+        return $this->id ;
+    }
+    
+    function getNom() {
+        return $this->nom ;
+    }
+    
+    
+    public static function getAllNom() {
+        try {
+            $database = Model::getInstance();
+            $query = "select nom from famille";
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+            return $results;
+        } 
+        catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+      
+        
+    public static function getAll() {
+        try {
+            $database = Model::getInstance();
+            $query = "select * from famille";
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelFamille");
+            return $results;
+        } 
+        catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+    
+    public static function getOne($nom) {
+        try {
+            $database = Model::getInstance();
+            $query = "select * from famille where nom = :nom";
+            $statement = $database->prepare($query);
+            $statement->execute([
+              'nom' => $nom
+            ]);
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelFamille");
+            return $results;
+        } 
+        catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+    
+    public static function insert($nom) {
+        try {
+            $database = Model::getInstance();
+
+            // recherche de la valeur de la clé = max(id) + 1
+            $query = "select max(id) from famille";
+            $statement = $database->query($query);
+            $tuple = $statement->fetch();
+            $id = $tuple['0'];
+            $id++;
+
+            // ajout d'un nouveau tuple;
+            $query = "insert into famille value (:id, :nom)";
+            $statement = $database->prepare($query);
+            $statement->execute([
+              'id' => $id,
+              'nom' => $nom
+            ]);
+            return $id;
+        } 
+        catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return -1;
+        }
+    }
+            
 }
+
 ?>
-<!-- ----- fin ModelFamille -->
+
+<!-- fin ModelFamille -->
