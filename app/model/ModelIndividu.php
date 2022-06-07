48148ed7f831c37936de9739f89cc80598e3346e
laurepg <106607375+laurepg@users.noreply.github.com>
2022-06-07 14:26:51 +0200

    Add files via upload

--- app/model/ModelIndividu.php
+++ app/model/ModelIndividu.php
@@ -5,86 +5,108 @@
 require_once 'Model.php';
 
 class ModelIndividu  {
-    private $famille_id,$id,$mere,$nom,$pere,$prenom,$sexe;
+    private $famille_id, $id, $nom, $prenom, $sexe, $pere, $mere ;
+    
     public function __construct($famille_id = NULL, $id = NULL, $mere = NULL, $nom = NULL, $pere=NULL,$prenom=NULL,$sexe=NULL) {
-  // valeurs nulles si pas de passage de parametres
-  if (!is_null($id)) {
-   $this->famille_id = $famille_id;
-   $this->id = $id;
-   $this->mere = $mere;
-   $this->nom = $nom;
-   $this->pere = $pere;
-   $this->prenom = $prenom;
-   $this->sexe = $sexe;
-  }
- }
+        // valeurs nulles si pas de passage de parametres
+        if (!is_null($id)) {
+            $this->famille_id = $famille_id;
+            $this->id = $id;
+            $this->mere = $mere;
+            $this->nom = $nom;
+            $this->pere = $pere;
+            $this->prenom = $prenom;
+            $this->sexe = $sexe;
+        }
+    }
 
- function setFamille_id($famille_id) {
-  $this->famille_id = $famille_id;
- }
+    function setFamille_id($famille_id) {
+        $this->famille_id = $famille_id;
+    }
 
- function setId($id) {
-  $this->id = $id;
- }
+    function setId($id) {
+        $this->id = $id;
+    }
 
- function setMere($mere) {
-  $this->mere = $mere;
- }
+    function setMere($mere) {
+        $this->mere = $mere;
+    }
 
- function setNom($nom) {
-  $this->nom = $nom;
- }
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function setPere($pere) {
+        $this->famille_id = $pere;
+    }
  
- function setPere($pere) {
-  $this->famille_id = $pere;
- }
- 
- function setPrenom($prenom) {
-  $this->prenom = $prenom;
- }
- function setSexe($sexe) {
-  $this->sexe = $sexe;
- }
- 
-function getFamille_id() {
-  return $this->famille_id;
- }
- 
- function getId() {
-  return $this->id;
- }
+    function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+    
+    function setSexe($sexe) {
+        $this->sexe = $sexe;
+    }
 
- function getMere() {
-  return $this->mere;
- }
+   function getFamille_id() {
+        return $this->famille_id;
+    }
 
- function getNom() {
-  return $this->nom;
- }
+    function getId() {
+        return $this->id;
+    }
 
- function getPere() {
-  return $this->pere;
- }
- function getPrenom() {
-  return $this->prenom;
- }
- function getSexe() {
-  return $this->sexe;
- }
- 
- public static function getAllByFamily() {
-  try {
-   $database = Model::getInstance();
-   $query = "select * from individu where famille_id=1002 ORDER BY id";
-   $statement = $database->prepare($query);
-   $statement->execute();
-   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelIndividu");
-   return $results;
-  } catch (PDOException $e) {
-   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
-   return NULL;
-  }
- }
+    function getMere() {
+        return $this->mere;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function getPere() {
+        return $this->pere;
+    }
+    
+    function getPrenom() {
+        return $this->prenom;
+    }
+    
+    function getSexe() {
+        return $this->sexe;
+    }
+
+    public static function getAll() {
+        try {
+            $database = Model::getInstance();
+            $query = "select * from individu where famille_id = :famille_id";
+            $statement = $database->prepare($query);
+            $statement->execute([
+                'famille_id' => $_SESSION["famille_id"]
+            ]);
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelIndividu");
+            return $results;
+        } 
+        catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+
+    public static function getAllByFamily() {
+     try {
+      $database = Model::getInstance();
+      $query = "select * from individu where famille_id=1002 ORDER BY id";
+      $statement = $database->prepare($query);
+      $statement->execute();
+      $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelIndividu");
+      return $results;
+     } catch (PDOException $e) {
+      printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+      return NULL;
+     }
+    }
  public static function insert() {
   try {
    $database = Model::getInstance();
